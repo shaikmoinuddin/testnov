d604de4f65c34457f745f4b617d53a7602fa710b
shaikmoinuddin <shaikmoin0303@gmail.com>
2022-05-10 10:01:02 +0530

    added

---
+++ intro.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## INTRODUCTION:
+
+# + python is a popular programming language.it was created by guido van Rossum in 1991.
+# + it is used for:
+# + web development(web server)
+# + software development.
+# + mathematics and system scripting.
+
+# ### what python can do?
+
+# + creates web applications.
+# + python can be used along side software to create workflows.
+# + python can connect the database systems.it can also read and modify the files.
+# + python can be used to handle big data and also perform complex mathematics.
+# + python is also used for rapid prototyping, or for production ready software development.
+
+# ### why python?
+
+# + python works on different platforms like windows,Mac,linux...etc.,
+# + python has simple syntax similar to english language.
+# + python has syntax which allows the developers to write programs with fewer lines than someother programming languages.
+# + python runs on interpreter it means it gets executed very soon.
+# + python can be treated in a procedural way, object-oriented or a functional way.
+
+# ### python syntax compared to other languages:
+
+# + python was designed for readability,have some similarities to english language with influence from mathematics.
+# + Python uses new lines to complete a command, as opposed to other programming languages which often use semicolons or parentheses.
+# + Python relies on indentation, using whitespace, to define scope; such as the scope of loops, functions and classes. Other programming languages often use curly-brackets for this purpose.
+
+# In[ ]:
+
+
+
+
